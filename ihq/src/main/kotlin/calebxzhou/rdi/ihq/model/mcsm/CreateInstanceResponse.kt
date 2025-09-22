7f9e3828c61b0823ec5cd3b1e0696477ad10c1bf
calebxzhou <me@calebxzhou.cn>
2025-09-22 23:45:48 +0800

    no mcsm

--- ihq/src/main/kotlin/calebxzhou/rdi/ihq/model/mcsm/CreateInstanceResponse.kt
+++
@@ -1,9 +0,0 @@
-package calebxzhou.rdi.ihq.model.mcsm
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CreateInstanceResponse(
-    val instanceUuid: String,
-    val config: InstanceConfig
-)
--- ihq/src/main/kotlin/calebxzhou/rdi/ihq/model/mcsm/UpdateInstanceResponse.kt
+++
@@ -1,8 +0,0 @@
-package calebxzhou.rdi.ihq.model.mcsm
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class UpdateInstanceResponse(
-    val instanceUuid: String
-)
