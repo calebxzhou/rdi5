96a457d29840925ba8a82527c57878ac12d1e515
calebxzhou <me@calebxzhou.cn>
2025-11-06 22:58:22 +0800

    +

---
+++ ihq/src/main/kotlin/calebxzhou/rdi/ihq/service/CurseForgeService.kt
@@ -0,0 +1,10 @@
+package calebxzhou.rdi.ihq.service
+
+import calebxzhou.rdi.ihq.model.pack.Mod
+
+object CurseForgeService {
+    const val BASE_URL ="https://api.curseforge.com/v1"
+    suspend fun download(mod: Mod){
+//todo
+    }
+}
