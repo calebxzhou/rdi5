5f4865b9a9485d09d1f3e3b0cdc41767fcd86b11
calebxzhou <me@calebxzhou.cn>
2025-11-06 22:58:31 +0800

    fonts

--- client/src/main/kotlin/calebxzhou/rdi/ui2/Fonts.kt
+++ client/src/main/kotlin/calebxzhou/rdi/ui2/Fonts.kt
@@ -4,7 +4,7 @@ import icyllis.modernui.text.Typeface
 
 enum class Fonts(val family: String) {
     UI("OPPO Sans 4.0"),
-    CODE("Sarasa Mono SC Nerd Font"),
+    CODE("JetBrains Mono"),
     ART("Smiley Sans Oblique"),
     ;
     val typeface
