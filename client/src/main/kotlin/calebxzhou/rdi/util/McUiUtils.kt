8b7cd049d2c3fd8bf92a445235f311a3daf8adec
calebxzhou <me@calebxzhou.cn>
2025-11-18 23:04:21 +0800

    rm mc gui funcs, no need

--- client/src/main/kotlin/calebxzhou/rdi/util/McUiUtils.kt
+++ client/src/main/kotlin/calebxzhou/rdi/util/McUiUtils.kt
@@ -1,6 +1,5 @@
 package calebxzhou.rdi.util
 
-import calebxzhou.rdi.common.WHITE
 import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.network.chat.Component
@@ -40,39 +39,3 @@ val MouseX
     get() = (mc.mouseHandler.xpos() * UiWidth / ScreenWidth).toInt()
 val MouseY
     get() = (mc.mouseHandler.ypos() * UiHeight / ScreenHeight).toInt()
-
-fun GuiGraphics.matrixOp(handler: PoseStack.() -> Unit) {
-    val stack = pose()
-    stack.matrixOp(handler)
-}
-
-fun PoseStack.matrixOp(handler: PoseStack.() -> Unit) {
-    pushPose()
-    handler(this)
-    popPose()
-}
-
-fun PoseStack.translate(x: Int, y: Int, z: Int) {
-    translate(x.toFloat(), y.toFloat(), z.toFloat())
-}
-fun PoseStack.scale(factor:Double){
-    scale(factor.toFloat(),factor.toFloat(),1f)
-}
-fun PoseStack.translate0() {
-    translate(0,0,1)
-}
-//平移到原点,缩放,再平移回去
-//字体缩小渲染用
-fun PoseStack.tran0ScaleBack(x:Int,y:Int,scaleFactor: Double){
-    translate0()
-    scale(scaleFactor)
-    translate(x/scaleFactor,y/scaleFactor,1.0)
-}
-fun GuiGraphics.drawText(
-    text: String="",
-    comp: Component = text.mcComp,
-    x: Int = 0,
-    y: Int= 0,
-    color: Int = WHITE,
-)=drawString(Font,comp,x,y,color,true)
-
