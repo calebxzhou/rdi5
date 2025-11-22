0ed730165cc6814a3373d87c061fee4a7e078242
calebxzhou <me@calebxzhou.cn>
2025-11-22 23:46:29 +0800

    combine

--- client/src/main/kotlin/calebxzhou/rdi/service/HostService.kt
+++ client/src/main/kotlin/calebxzhou/rdi/service/HostService.kt
@@ -2,6 +2,21 @@ package calebxzhou.rdi.service
 
 import calebxzhou.rdi.model.Host
 import calebxzhou.rdi.model.RAccount
+import calebxzhou.rdi.model.Role
 import calebxzhou.rdi.net.server
+import org.bson.types.ObjectId
 
+/**
+ * calebxzhou @ 2025-10-06 19:38
+ */
+fun Host.hasMember(id: ObjectId): Boolean {
+    return members.any { it.id == id }
+}
+val Host.owner
+    get() =  members.find { it.role== Role.OWNER }
+fun Host.isOwner(acc: RAccount) = owner?.id == acc._id
+// Check whether a user is OWNER or ADMIN of this team
+fun Host.isOwnerOrAdmin(acc: RAccount): Boolean = isOwnerOrAdmin(acc._id)
+fun Host.isOwnerOrAdmin(uid: ObjectId): Boolean =
+    members.any { it.id == uid && (it.role == Role.OWNER || it.role == Role.ADMIN) }
 suspend fun RAccount.myTeamHosts() = server.makeRequest<List<Host>>("host/").data
