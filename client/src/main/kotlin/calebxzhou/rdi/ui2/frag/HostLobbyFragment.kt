a6b51e481115096b95f56a64aeec633c18417f6a
calebxzhou <me@calebxzhou.cn>
2025-11-23 18:59:07 +0800

    lobby show

--- client/src/main/kotlin/calebxzhou/rdi/ui2/frag/HostLobbyFragment.kt
+++ client/src/main/kotlin/calebxzhou/rdi/ui2/frag/HostLobbyFragment.kt
@@ -1,20 +1,33 @@
 package calebxzhou.rdi.ui2.frag
 
 import calebxzhou.rdi.Const
+import calebxzhou.rdi.auth.LocalCredentials
 import calebxzhou.rdi.model.Host
+import calebxzhou.rdi.model.HostStatus
 import calebxzhou.rdi.net.server
 import calebxzhou.rdi.ui2.*
 import calebxzhou.rdi.ui2.component.HostGrid
+import calebxzhou.rdi.ui2.component.alertErr
+import calebxzhou.rdi.ui2.component.alertOk
+import calebxzhou.rdi.util.ioTask
+import calebxzhou.rdi.util.isMcStarted
+import calebxzhou.rdi.util.mc
+import calebxzhou.rdi.util.renderThread
+import net.minecraft.client.gui.screens.ConnectScreen
+import net.minecraft.client.gui.screens.Screen
+import net.minecraft.client.multiplayer.resolver.ServerAddress
+import icyllis.modernui.view.Gravity
 import org.bson.types.ObjectId
 
 class HostLobbyFragment : RFragment("服务器大厅") {
-
-    private lateinit var hostGrid: HostGrid
+    companion object {
+        var screen: Screen? = null
+    }
 
     init {
         contentViewInit = {
-            hostGrid = HostGrid(context)
-            addView(hostGrid, linearLayoutParam(PARENT, PARENT))
+            if (isMcStarted)
+                screen = this@HostLobbyFragment.mcScreen
             loadHosts(false)
         }
         titleViewInit = {
@@ -39,13 +52,57 @@ class HostLobbyFragment : RFragment("服务器大厅") {
             return
         }
 
-        server.request<List<Host.Vo>>("host/${if(my) "my" else "lobby/0"}") { response ->
+        server.request<List<Host.Vo>>("host/${if (my) "my" else "lobby/0"}") { response ->
             response.data?.let { showHosts(it) }
         }
     }
 
     private fun showHosts(hosts: List<Host.Vo>) = uiThread {
-        hostGrid.hosts = hosts
+        contentView.removeAllViews()
+        if (hosts.isEmpty()) {
+            contentView.textView("暂无可展示的主机") {
+                gravity = Gravity.CENTER
+                setTextColor(MaterialColor.GRAY_400.colorValue)
+                textSize = 16f
+                paddingDp(0, 32, 0, 32)
+            }
+            return@uiThread
+        }
+        contentView += HostGrid(contentView.context, hosts, { HostInfoFragment(it._id).go() }, {
+            if (isMcStarted) {
+                it.play()
+            }
+        })
+    }
+
+    private fun Host.Vo.play() {
+        //电信以外全bgp
+        val bgp = LocalCredentials.read().carrier != 0
+        server.request<HostStatus>("host/${_id}/status"){
+            val status = it.data
+            if (status == HostStatus.STARTED) {
+                alertErr("主机正在载入中\n请稍等1~5分钟")
+                return@request
+            } else if (status == HostStatus.STOPPED) {
+                server.requestU("host/${_id}/start") {
+                    alertOk("主机已经启动\n请稍等1~5分钟")
+                    return@requestU
+                }
+            } else if(status == HostStatus.PLAYABLE){
+                Host.portNow = this.port
+                renderThread {
+                    ConnectScreen.startConnecting(
+                        this@HostLobbyFragment.mcScreen,
+                        mc,
+                        ServerAddress(if (bgp) server.bgpIp else server.ip, server.gamePort),
+                        server.mcData(bgp),
+                        false,
+                        null
+                    )
+                }
+            }
+        }
+
     }
 
     private fun generateMockHosts(): List<Host.Vo> = List(50) { index ->
