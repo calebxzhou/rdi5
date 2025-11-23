a1b7b6fdbf93173356770a26260fdb26b5d864b6
calebxzhou <me@calebxzhou.cn>
2025-11-23 19:10:45 +0800

    world for person

--- client/src/main/kotlin/calebxzhou/rdi/model/World.kt
+++ client/src/main/kotlin/calebxzhou/rdi/model/World.kt
@@ -8,10 +8,10 @@ import org.bson.types.ObjectId
 data class World(
     @Contextual val _id: ObjectId = ObjectId(),
     val name: String,
-    //所属团队
+    //所属玩家
     @Contextual
-    val teamId: ObjectId,
-    //如果mount了与上次不同的modpack  警告用户可能坏档
+    val ownerId: ObjectId,
+    //如果mount不同的modpack  警告用户可能坏档
     @Contextual
     val modpackId: ObjectId,
 
