46932af9fc17e1a057a2c33743ecbe6f58618fa9
calebxzhou <me@calebxzhou.cn>
2025-11-26 09:32:50 +0800

    no need plugin, combine into service

--- ihq/src/main/kotlin/calebxzhou/rdi/ihq/service/PlayerPlugin.kt
+++
@@ -1,8 +0,0 @@
-package calebxzhou.rdi.ihq.service
-
-import calebxzhou.rdi.ihq.exception.RequestError
-import calebxzhou.rdi.ihq.model.RAccount
-import calebxzhou.rdi.ihq.net.uid
-import io.ktor.server.application.ApplicationCall
-
-suspend fun ApplicationCall.player(): RAccount = PlayerService.getById(uid)?: throw RequestError("用户不存在")
