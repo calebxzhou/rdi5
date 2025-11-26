285dc6fab6d40c7825302508c3bd20b87b1594c5
calebxzhou <me@calebxzhou.cn>
2025-11-26 09:33:04 +0800

    see my packs

--- client/src/main/kotlin/calebxzhou/rdi/ui2/frag/ModpackListFragment.kt
+++ client/src/main/kotlin/calebxzhou/rdi/ui2/frag/ModpackListFragment.kt
@@ -1,5 +1,6 @@
 package calebxzhou.rdi.ui2.frag
 
+import calebxzhou.rdi.model.account
 import calebxzhou.rdi.model.pack.ModpackVo
 import calebxzhou.rdi.net.server
 import calebxzhou.rdi.ui2.*
@@ -15,17 +16,19 @@ class ModpackListFragment: RFragment("大家的整合包") {
         }
         titleViewInit = {
             quickOptions {
-                "\uDB80\uDFD5 传包" colored MaterialColor.BLUE_900 with { ModpackUploadFragment().go() }
-               // "\uEB29 我的包" colored MaterialColor.TEAL_900 with { ModpackCreateFragment().go() }
+                "\uDB81\uDC8B 我的包" make checkbox with {
+                    load { it.authorId== account._id }
+                }
+                "\uDB80\uDFD5 上传整合包" colored MaterialColor.BLUE_900 with { ModpackUploadFragment().go() }
                 "\uDB86\uDDD8 做新包" colored MaterialColor.AMBER_900 with { alertErr("没写完") }
             }
         }
     }
-    private fun load() = ioTask {
+    private fun load(filter: (ModpackVo) -> Boolean = {true} ) = ioTask {
         server.makeRequest<List<ModpackVo>>("modpack").data?.let {
             uiThread {
                 contentView.apply {
-                    this += ModpackGrid(context,it){
+                    this += ModpackGrid(context,it.filter(filter)){
                         ModpackInfoFragment(it.id).go()
                     }
                 }
