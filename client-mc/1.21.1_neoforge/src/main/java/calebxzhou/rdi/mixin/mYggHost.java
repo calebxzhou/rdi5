d66d2c1860f5c5a3e779143576e79d8bc50bfb02
calebxzhou <me@calebxzhou.cn>
2025-12-14 23:38:01 +0800

    impl ygg in rdi

---
+++ client-mc/1.21.1_neoforge/src/main/java/calebxzhou/rdi/mixin/mYggHost.java
@@ -0,0 +1,18 @@
+package calebxzhou.rdi.mixin;
+
+import calebxzhou.rdi.RDI;
+import com.mojang.authlib.Environment;
+import com.mojang.authlib.yggdrasil.YggdrasilEnvironment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+/**
+ * calebxzhou @ 2025-12-14 21:39
+ */
+@Mixin(YggdrasilEnvironment.class)
+public class mYggHost {
+    @Overwrite(remap = false)
+    public Environment getEnvironment() {
+        return new Environment(RDI.IHQ_URL,RDI.IHQ_URL,"PROD");
+    }
+}
