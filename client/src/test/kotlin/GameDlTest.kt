fdba918cb37e512cf7cea5f0d4244b34238143dc
calebxzhou <me@calebxzhou.cn>
2025-12-14 23:36:28 +0800

    start pack test

--- client/src/test/kotlin/GameDlTest.kt
+++ client/src/test/kotlin/GameDlTest.kt
@@ -1,8 +1,11 @@
 import calebxzhou.rdi.model.McVersion
 import calebxzhou.rdi.model.ModLoader
+import calebxzhou.rdi.model.pack.Modpack
 import calebxzhou.rdi.service.GameService
 import calebxzhou.rdi.service.GameService.rewriteMirrorUrl
+import calebxzhou.rdi.service.ModpackService
 import kotlinx.coroutines.runBlocking
+import org.bson.types.ObjectId
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -31,6 +34,14 @@ class GameDlTest {
         GameService.start(McVersion.V211, "neoforge-21.1.216") { println(it) }
     }
     @Test
+    fun start20(): Unit = runBlocking {
+        GameService.start(McVersion.V201, "1.20.1-forge-47.4.13") { println(it) }
+    }
+    @Test
+    fun startModpack(): Unit = runBlocking {
+        GameService.start(McVersion.V211, "693bda0ed294de2450aa7caf_1.9.2") { println(it) }
+    }
+    @Test
     fun installLoader21(): Unit = runBlocking {
         GameService.downloadLoader(McVersion.V211, ModLoader.NEOFORGE) { println(it) }
     }
@@ -39,4 +50,5 @@ class GameDlTest {
     fun installLoader20(): Unit = runBlocking {
         GameService.downloadLoader(McVersion.V201, ModLoader.FORGE) { println(it) }
     }
+
 }
