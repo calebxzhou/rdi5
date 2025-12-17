b44be6f830ad26a49146c6ab49fa20ffab7ed8f3
calebxzhou <me@calebxzhou.cn>
2025-12-17 11:31:09 +0800

    check class loading

--- client-mc/1.21.1_neoforge/src/main/kotlin/calebxzhou/rdi/client/mc/Utils.kt
+++ client-mc/1.21.1_neoforge/src/main/kotlin/calebxzhou/rdi/client/mc/Utils.kt
@@ -1,5 +1,7 @@
 package calebxzhou.rdi.client.mc
 
+import java.util.UUID
+
 /**
  * calebxzhou @ 2025-12-15 12:03
  */
@@ -7,3 +9,11 @@ fun String.camelToSnakeCase(): String =
     replace(Regex("([a-z0-9])([A-Z])"), "$1_$2")
         .replace("-", "_")
         .lowercase()
+fun isClassLoaded(className: String, classLoader: ClassLoader = ClassLoader.getSystemClassLoader()): Boolean {
+    return try {
+        Class.forName(className, false, classLoader) // false = do not initialize
+        true
+    } catch (e: ClassNotFoundException) {
+        false
+    }
+}
