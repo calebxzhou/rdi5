f499a5bbbd0075e9d918ecc56aa96c052df20019
calebxzhou <me@calebxzhou.cn>
2025-12-24 23:31:04 +0800

    rm test

--- client/ui/src/main/kotlin/calebxzhou/rdi/service/ChatService.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/service/ChatService.kt
@@ -1,50 +1,12 @@
 package calebxzhou.rdi.service
 
-import calebxzhou.rdi.Const
+import calebxzhou.rdi.common.model.ChatMsg
+import calebxzhou.rdi.common.serdesJson
 import calebxzhou.rdi.lgr
-import calebxzhou.rdi.model.ChatMsg
-import calebxzhou.rdi.model.RAccount
 import calebxzhou.rdi.net.server
-import calebxzhou.rdi.util.ioTask
-import calebxzhou.rdi.util.json
-import calebxzhou.rdi.util.serdesJson
 import io.ktor.http.*
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 
-
-suspend fun main() {
-    RAccount.now = RAccount.TESTS[0]
-    Const.DEBUG=true
-    server.sse("chat/listen",
-        onEvent = { event ->
-            when (event.event) {
-                "heartbeat" -> return@sse
-                "error" -> {
-                    val message = event.data?.ifBlank { null } ?: "unknown"
-                    lgr.error ( "Host log stream error event: $message" )
-                    return@sse
-                }
-            }
-            val payload = event.data?.ifBlank { null } ?: return@sse
-            val msg = serdesJson.decodeFromString<ChatMsg.Dto>(payload)
-            lgr.info(msg.json)
-        },
-        onClosed = {
-            lgr.info("已关闭日志流")
-        },
-        onError = { throwable ->
-            lgr.error{throwable}
-        })
-    RAccount.now = RAccount.TESTS[1]
-    ioTask {
-        for (i in 1000..10000){
-            RAccount.now = RAccount.TESTS.random()
-            server.makeRequest<Unit>("chat/send", HttpMethod.Post,mapOf("content" to "${i}")){}
-            delay(10000)
-        }
-    }.join()
-}
 object ChatService {
     //0-全局聊天 1-岛内成员聊天
     var chatMode = 0
