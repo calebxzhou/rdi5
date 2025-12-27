0d0e2390fce375c2cbe4118e7f4e8d477375d583
calebxzhou <me@calebxzhou.cn>
2025-12-27 23:58:58 +0800

    merge into modpack service

--- client/ui/src/main/kotlin/calebxzhou/rdi/service/ModpackService.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/service/ModpackService.kt
@@ -1,14 +1,26 @@
 package calebxzhou.rdi.service
 
 import calebxzhou.mykotutils.curseforge.CFDownloadMod
+import calebxzhou.mykotutils.curseforge.CFDownloadModException
 import calebxzhou.mykotutils.curseforge.CurseForgeApi
+import calebxzhou.mykotutils.std.humanFileSize
 import calebxzhou.mykotutils.std.openChineseZip
 import calebxzhou.mykotutils.std.sha1
 import calebxzhou.mykotutils.std.toFixed
 import calebxzhou.rdi.RDI
+import calebxzhou.rdi.auth.LocalCredentials
 import calebxzhou.rdi.common.DL_MOD_DIR
+import calebxzhou.rdi.common.model.Host
+import calebxzhou.rdi.common.model.HostStatus
 import calebxzhou.rdi.common.model.Mod
+import calebxzhou.rdi.common.model.Modpack
+import calebxzhou.rdi.common.util.ioTask
 import calebxzhou.rdi.net.server
+import calebxzhou.rdi.ui2.component.alertErr
+import calebxzhou.rdi.ui2.component.alertOk
+import calebxzhou.rdi.ui2.component.confirm
+import calebxzhou.rdi.ui2.frag.TaskFragment
+import calebxzhou.rdi.ui2.go
 import calebxzhou.rdi.ui2.pointerBuffer
 import org.bson.types.ObjectId
 import org.lwjgl.util.tinyfd.TinyFileDialogs
@@ -18,28 +30,50 @@ import kotlin.io.path.name
 
 val selectModpackFile
     get() = TinyFileDialogs.tinyfd_openFileDialog(
-    "选择整合包 (ZIP)",
-    "C:/Users/${System.getProperty("user.name")}/Downloads",
-    ("*.zip").pointerBuffer,
-    "CurseForge整合包 (*.zip)",
-    false
-)
+        "选择整合包 (ZIP)",
+        "C:/Users/${System.getProperty("user.name")}/Downloads",
+        ("*.zip").pointerBuffer,
+        "CurseForge整合包 (*.zip)",
+        false
+    )
+
 object ModpackService {
-    suspend fun installVersion(modpackId: ObjectId, verName: String, mods: List<Mod>, onProgress: (String) -> Unit){
-        val versionDir = GameService.versionListDir.resolve("${modpackId}_${verName}")
+    fun getVersionDir(modpackId: ObjectId, verName: String): File {
+        return GameService.versionListDir.resolve("${modpackId}_${verName}")
+    }
+
+    suspend fun installVersion(modpackId: ObjectId, verName: String, mods: List<Mod>, onProgress: (String) -> Unit) {
+        val versionDir = getVersionDir(modpackId, verName)
         val mods = CurseForgeApi.downloadMods(
             mods.map {
-                CFDownloadMod(it.projectId.toInt(), it.fileId.toInt(),it.slug, DL_MOD_DIR.resolve(it.fileName).toPath())
+                CFDownloadMod(
+                    it.projectId.toInt(),
+                    it.fileId.toInt(),
+                    it.slug,
+                    DL_MOD_DIR.resolve(it.fileName).toPath()
+                )
             }
-        ){ cfmod,prog->
+        ) { cfmod, prog ->
             onProgress("mod下载中：${cfmod.slug} ${prog.percent.toFixed(2)}")
-        }.getOrThrow()
-        val clientPack = downloadVersionClientPack(modpackId,verName,onProgress)
-        if(clientPack==null){
+        }.getOrElse {
+            if (it is CFDownloadModException) {
+                it.failed.forEach { (mod, ex) ->
+                    onProgress("Mod ${mod} 下载失败:${ex.message}，安装终止")
+                    ex.printStackTrace()
+                }
+            } else {
+                it.printStackTrace()
+                onProgress("未知错误:${it.message}，安装终止")
+
+            }
+            return
+        }
+        val clientPack = downloadVersionClientPack(modpackId, verName, onProgress)
+        if (clientPack == null) {
             onProgress("客户端包下载失败，安装终止")
             return
         }
-        if(versionDir.exists()){
+        if (versionDir.exists()) {
             versionDir.deleteRecursively()
         }
         versionDir.mkdirs()
@@ -68,22 +102,69 @@ object ModpackService {
         }
         onProgress("整合包安装完成 位于:${versionDir.absolutePath}")
     }
-    suspend fun downloadVersionClientPack(modpackId: ObjectId,verName: String,onProgress: (String) -> Unit): File? {
+
+    suspend fun downloadVersionClientPack(modpackId: ObjectId, verName: String, onProgress: (String) -> Unit): File? {
         val file = RDI.DIR.resolve("${modpackId}_$verName.zip")
         val hash = server.makeRequest<String>("modpack/$modpackId/version/$verName/client/hash").data
-        if(file.exists() && file.sha1 == hash){
+        if (file.exists() && file.sha1 == hash) {
             return file
         }
         onProgress("下载客户端整合包...")
-        server.download("modpack/$modpackId/version/$verName/client", file.toPath()){
+        server.download("modpack/$modpackId/version/$verName/client", file.toPath()) {
             onProgress(it.percent.toFixed(2))
         }
 
-        if(hash != file.sha1){
+        if (hash != file.sha1) {
             onProgress("文件损坏了，请重新下载")
             file.delete()
             return null
         }
         return file
     }
+    fun Modpack.Version.startInstall(modpackName: String?=null) {
+        TaskFragment("下载整合包 ${modpackName?:""} ${totalSize?.humanFileSize} ") {
+            ModpackService.installVersion(modpackId, name, mods) { progress ->
+                this.log(progress)
+            }
+        }.go()
+    }
+    fun isVersionInstalled(modpackId: ObjectId, verName: String): Boolean {
+        val versionDir = getVersionDir(modpackId, verName)
+        if (!versionDir.exists()) return false
+        versionDir.resolve("mods").takeIf { it.exists() } ?: return false
+        versionDir.resolve("config").takeIf { it.exists() } ?: return false
+        return true
+    }
+    fun Host.startPlay() = ioTask {
+        val status = server.makeRequest<HostStatus>("host/${_id}/status").data ?: let {
+            alertErr("获取房间状态失败，无法游玩")
+            return@ioTask
+        }
+        if(!isVersionInstalled(modpackId, packVer)){
+            confirm("未下载此房间的整合包，无法游玩。要现在开始下载吗？"){
+                //todo dl version
+                server.makeRequest<Modpack.Version>()
+                GameService.startInstallModpackVersion(modpackId, packVer)
+            }
+            return@ioTask
+        }
+        if (status != HostStatus.PLAYABLE) {
+            when (status) {
+                HostStatus.STARTED -> {
+                    alertErr("房间正在载入中\n请稍等1~2分钟")
+                }
+                HostStatus.STOPPED -> {
+                    server.requestU("host/${_id}/start") {
+                        alertOk("房间已经启动\n请稍等1~2分钟\n可以在“后台”查看启动状态")
+                    }
+                }
+                else -> {
+                    alertErr("房间状态未知，无法游玩")
+                }
+            }
+            return@ioTask
+        }
+        val bgp = LocalCredentials.read().carrier != 0
+    }
+}
 }
