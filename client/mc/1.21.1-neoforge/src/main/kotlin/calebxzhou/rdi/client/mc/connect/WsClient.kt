3f818b20acbf7bee33a30e6b1e3bf52f1705dfd8
calebxzhou <me@calebxzhou.cn>
2025-12-27 23:58:43 +0800

    lgrs

--- client/mc/1.21.1-neoforge/src/main/kotlin/calebxzhou/rdi/client/mc/connect/WsClient.kt
+++ client/mc/1.21.1-neoforge/src/main/kotlin/calebxzhou/rdi/client/mc/connect/WsClient.kt
@@ -1,5 +1,6 @@
 package calebxzhou.rdi.client.mc.connect
 
+import calebxzhou.mykotutils.log.Loggers
 import calebxzhou.rdi.client.mc.*
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
@@ -11,6 +12,7 @@ import kotlinx.coroutines.launch
 typealias WsSession = DefaultClientWebSocketSession
 var wsSession: WsSession? = null
 object WsClient {
+    private val lgr by Loggers
     val wsClient = HttpClient(OkHttp){
         install(WebSockets){
             contentConverter = KotlinxWebsocketSerializationConverter(serdesJson)
@@ -19,7 +21,7 @@ object WsClient {
     }
     suspend fun connectCore(){
         wsClient.webSocket("ws://127.0.0.1:65240/") {
-            lgr.info("rdi core connected")
+            lgr.info { "rdi core connected" }
             wsSession = this
             handleMsg()
         }
@@ -29,13 +31,13 @@ object WsClient {
             val msg = try {
                 receiveDeserialized<WsMessage>()
             } catch (closed: ClosedReceiveChannelException) {
-                lgr.info("WebSocket channel closed, stop receiving commands")
+                lgr.info { "WebSocket channel closed, stop receiving commands" }
                 return
             } catch (throwable: Throwable) {
-                lgr.error("Failed to parse command", throwable)
+                lgr.error(throwable) { "${"Failed to parse command"}" }
                 return
             }
-            lgr.info("incoming msg: ${msg.json}")
+            lgr.info { "incoming msg: ${msg.json}" }
             msg.handle(this)
         }
     }
@@ -45,7 +47,7 @@ object WsClient {
     suspend fun <T> WsSession.response(reqId: Int,msg: T){ sendMessage(Response(reqId,msg)) }
     suspend fun WsSession.sendMessage(msg: WsMessage) {
         val msg = serdesJson.encodeToString<WsMessage>(msg)
-        lgr.info("msg send: $msg")
+        lgr.info { "msg send: $msg" }
         send(Frame.Text(msg))
     }
 }
