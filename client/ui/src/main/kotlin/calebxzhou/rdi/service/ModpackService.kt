2e7c1e5d614eb38c29ffe7464df7312624f2b6c7
calebxzhou <me@calebxzhou.cn>
2025-12-29 19:43:08 +0800

    complete run & install

--- client/ui/src/main/kotlin/calebxzhou/rdi/service/ModpackService.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/service/ModpackService.kt
@@ -13,8 +13,13 @@ import calebxzhou.rdi.common.DL_MOD_DIR
 import calebxzhou.rdi.common.model.Host
 import calebxzhou.rdi.common.model.HostStatus
 import calebxzhou.rdi.common.model.Mod
+import calebxzhou.rdi.common.model.ModLoader
 import calebxzhou.rdi.common.model.Modpack
+import calebxzhou.rdi.common.model.ModpackDetailedVo
+import calebxzhou.rdi.common.model.ModpackVo
 import calebxzhou.rdi.common.util.ioTask
+import calebxzhou.rdi.common.util.str
+import calebxzhou.rdi.model.McVersion
 import calebxzhou.rdi.net.server
 import calebxzhou.rdi.ui2.component.alertErr
 import calebxzhou.rdi.ui2.component.alertOk
@@ -22,10 +27,12 @@ import calebxzhou.rdi.ui2.component.confirm
 import calebxzhou.rdi.ui2.frag.TaskFragment
 import calebxzhou.rdi.ui2.go
 import calebxzhou.rdi.ui2.pointerBuffer
+import kotlinx.coroutines.runBlocking
 import org.bson.types.ObjectId
 import org.lwjgl.util.tinyfd.TinyFileDialogs
 import java.io.File
 import java.nio.file.Files
+import kotlin.collections.plusAssign
 import kotlin.io.path.name
 
 val selectModpackFile
@@ -42,7 +49,7 @@ object ModpackService {
         return GameService.versionListDir.resolve("${modpackId}_${verName}")
     }
 
-    suspend fun installVersion(modpackId: ObjectId, verName: String, mods: List<Mod>, onProgress: (String) -> Unit) {
+    suspend fun installVersion(mcVersion: McVersion,modLoader: ModLoader,modpackId: ObjectId, verName: String, mods: List<Mod>, onProgress: (String) -> Unit) {
         val versionDir = getVersionDir(modpackId, verName)
         val mods = CurseForgeApi.downloadMods(
             mods.map {
@@ -100,6 +107,16 @@ object ModpackService {
         mods.forEach { dlmod ->
             Files.createSymbolicLink(modsDir.resolve(dlmod.path.fileName.name).toPath(), dlmod.path)
         }
+        val mcSlug = "${mcVersion.mcVer}-${modLoader.name.lowercase()}"
+        val mcCoreTarget = DL_MOD_DIR.resolve("rdi-5-mc-client-$mcSlug.jar").toPath()
+        val mcCoreLink = modsDir.resolve("rdi-5-mc-client-$mcSlug.jar").toPath()
+        if (mcCoreTarget.toFile().exists()) {
+            Files.deleteIfExists(mcCoreLink)
+            Files.createSymbolicLink(mcCoreLink, mcCoreTarget)
+        } else {
+            onProgress("缺少核心文件: ${mcCoreTarget.toFile().absolutePath}")
+            return
+        }
         onProgress("整合包安装完成 位于:${versionDir.absolutePath}")
     }
 
@@ -121,13 +138,22 @@ object ModpackService {
         }
         return file
     }
-    fun Modpack.Version.startInstall(modpackName: String?=null) {
-        TaskFragment("下载整合包 ${modpackName?:""} ${totalSize?.humanFileSize} ") {
-            ModpackService.installVersion(modpackId, name, mods) { progress ->
+
+    fun Modpack.Version.startInstall(mcVer: String, modLoaderStr: String, modpackName: String? = null) {
+        val mcVersion = McVersion.from(mcVer)
+        val modLoader = runCatching { ModLoader.valueOf(modLoaderStr.uppercase()) }.getOrNull()
+        if (mcVersion == null || modLoader == null) {
+            alertErr("不支持的版本或加载器: $mcVer $modLoaderStr")
+            return
+        }
+
+        TaskFragment("下载整合包 ${modpackName ?: ""} ${totalSize?.humanFileSize} ") {
+            ModpackService.installVersion(mcVersion, modLoader, modpackId, this@startInstall.name, mods) { progress ->
                 this.log(progress)
             }
         }.go()
     }
+
     fun isVersionInstalled(modpackId: ObjectId, verName: String): Boolean {
         val versionDir = getVersionDir(modpackId, verName)
         if (!versionDir.exists()) return false
@@ -135,16 +161,31 @@ object ModpackService {
         versionDir.resolve("config").takeIf { it.exists() } ?: return false
         return true
     }
+
     fun Host.startPlay() = ioTask {
-        val status = server.makeRequest<HostStatus>("host/${_id}/status").data ?: let {
-            alertErr("获取房间状态失败，无法游玩")
-            return@ioTask
+        val status = server.makeRequest<HostStatus>("host/${_id}/status").run {
+            data ?: run {
+                alertErr("获取房间状态失败，无法游玩: ${this.msg}")
+                return@ioTask
+            }
         }
-        if(!isVersionInstalled(modpackId, packVer)){
-            confirm("未下载此房间的整合包，无法游玩。要现在开始下载吗？"){
-                //todo dl version
-                server.makeRequest<Modpack.Version>()
-                GameService.startInstallModpackVersion(modpackId, packVer)
+        val modpack = server.makeRequest<ModpackDetailedVo>("modpack/${modpackId}").run {
+            data ?: run {
+                alertErr("获取房间整合包信息失败，无法游玩: ${this.msg}")
+                return@ioTask
+            }
+        }
+        val version = server.makeRequest<Modpack.Version>("modpack/$modpackId/version/$packVer").run {
+            data ?: run {
+                alertErr("获取整合包版本信息失败，无法游玩: ${this.msg}")
+                return@ioTask
+            }
+        }
+        if (!isVersionInstalled(modpackId, packVer)) {
+            confirm("未下载此房间的整合包，无法游玩。要现在开始下载吗？") {
+                ioTask {
+                    version.startInstall(modpack.mcVer, modpack.modloader)
+                }
             }
             return@ioTask
         }
@@ -153,11 +194,13 @@ object ModpackService {
                 HostStatus.STARTED -> {
                     alertErr("房间正在载入中\n请稍等1~2分钟")
                 }
+
                 HostStatus.STOPPED -> {
                     server.requestU("host/${_id}/start") {
                         alertOk("房间已经启动\n请稍等1~2分钟\n可以在“后台”查看启动状态")
                     }
                 }
+
                 else -> {
                     alertErr("房间状态未知，无法游玩")
                 }
@@ -165,6 +208,19 @@ object ModpackService {
             return@ioTask
         }
         val bgp = LocalCredentials.read().carrier != 0
+        McVersion.from(modpack.mcVer)?.let { mcVersion ->
+            TaskFragment("启动mc中") {
+                GameService.start(
+                    mcVersion, "${modpackId.str}_${version.name}",
+                    "-Drdi.ihq.url=${server.hqUrl}",
+                    "-Drdi.game.ip=${if (bgp) server.bgpIp else server.ip}:65230",
+                    "-Drdi.host.name=${this@startPlay.name}",
+                    "-Drdi.host.port=${this@startPlay.port}"
+                ) {
+                    this.log(it)
+                }
+            }.go()
+        } ?: alertErr("不支持的MC版本:${modpack.mcVer}  无法游玩")
+
     }
-}
 }
