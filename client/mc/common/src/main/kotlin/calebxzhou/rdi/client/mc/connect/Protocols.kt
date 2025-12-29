0183cf791f69990bb066dfbd940afdd46eba51f7
calebxzhou <me@calebxzhou.cn>
2025-12-29 23:01:21 +0800

    useless

--- client/mc/common/src/main/kotlin/calebxzhou/rdi/client/mc/connect/Protocols.kt
+++
@@ -1,7 +0,0 @@
-package calebxzhou.rdi.client.mc.connect
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-/**
- * calebxzhou @ 2025-12-17 0:06
- */
-@Serializable sealed class WsMessage
