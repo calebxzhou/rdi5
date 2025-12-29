44d029f8c4e000d6f28bc66c14982e69cb1312a2
calebxzhou <me@calebxzhou.cn>
2025-12-29 19:45:47 +0800

    update api

--- server/master/src/main/kotlin/calebxzhou/rdi/master/service/UpdateService.kt
+++ server/master/src/main/kotlin/calebxzhou/rdi/master/service/UpdateService.kt
@@ -1,25 +1,36 @@
 package calebxzhou.rdi.master.service
 
+import calebxzhou.mykotutils.std.sha1
 import calebxzhou.rdi.master.exception.RequestError
+import calebxzhou.rdi.master.net.param
 import calebxzhou.rdi.master.net.response
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
 import java.util.jar.JarFile
 
-private val coreFile
-    get() = File("rdi-5-client.jar")
+private val CLIENT_LIBS_DIR = File("client-libs").also { it.mkdirs() }
+
+private val uiFile
+    get() = CLIENT_LIBS_DIR.resolve("rdi-5-ui.jar")
+private val mcCoreFilePrefix = "rdi-5-mc-client"
+
 fun Route.updateRoutes() = route("/update") {
-    get("/ver") {
-        if (!coreFile.exists()) throw RequestError("客户端rdi核心维护中")
-        val version = JarFile(coreFile).use { jar ->
-            jar.manifest?.mainAttributes?.getValue("Implementation-Version")
-        }?.takeIf { it.isNotBlank() }
-            ?: throw RequestError("无法读取客户端版本号")
-        response(data = mapOf("version" to version))
+    get("/ui/hash") {
+        response(data = uiFile.sha1)
     }
-    get("/core") {
-        if (!coreFile.exists()) throw RequestError("客户端rdi核心维护中")
-        call.respondFile(coreFile)
+    get("/ui") {
+        if (!uiFile.exists()) throw RequestError("客户端rdi核心维护中")
+        call.respondFile(uiFile)
+    }
+    get("/mc/{ver}/hash"){
+        val jarFile = CLIENT_LIBS_DIR.resolve("$mcCoreFilePrefix-${param("ver")}.jar")
+        if(!jarFile.exists()) throw RequestError("无此版本的MC核心库")
+        response(data = jarFile.sha1)
+    }
+    get("/mc/{ver}"){
+        val jarFile = CLIENT_LIBS_DIR.resolve("$mcCoreFilePrefix-${param("ver")}.jar")
+        if(!jarFile.exists()) throw RequestError("无此版本的MC核心库")
+        call.respondFile(jarFile)
     }
 }
