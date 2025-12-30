4bb5dfee925b00e7bf2eb12d9f400647d126256a
calebxzhou <me@calebxzhou.cn>
2025-12-30 19:02:06 +0800

    disable 201 temp

--- client/ui/src/test/kotlin/GameDlTest.kt
+++ client/ui/src/test/kotlin/GameDlTest.kt
@@ -21,19 +21,19 @@ class GameDlTest {
         GameService.downloadVersion(McVersion.V211) { println(it) }
     }
 
-    @Test
+    /*@Test
     fun dl20(): Unit = runBlocking {
 
         GameService.downloadVersion(McVersion.V201) { println(it) }
-    }
+    }*/
     @Test
     fun start21(): Unit = runBlocking {
         GameService.start(McVersion.V211, "neoforge-21.1.216") { println(it) }
     }
-    @Test
+    /*@Test
     fun start20(): Unit = runBlocking {
         GameService.start(McVersion.V201, "1.20.1-forge-47.4.13") { println(it) }
-    }
+    }*/
     @Test
     fun startModpack(): Unit = runBlocking {
         GameService.start(McVersion.V211, "693bda0ed294de2450aa7caf_1.9.2") { println(it) }
@@ -43,9 +43,9 @@ class GameDlTest {
         GameService.downloadLoader(McVersion.V211, ModLoader.NEOFORGE) { println(it) }
     }
 
-    @Test
+    /*@Test
     fun installLoader20(): Unit = runBlocking {
         GameService.downloadLoader(McVersion.V201, ModLoader.FORGE) { println(it) }
-    }
+    }*/
 
 }
