964886e574176edf7c21e20dc51cf60efa67b684
calebxzhou <me@calebxzhou.cn>
2025-12-30 19:01:46 +0800

    disable 201 temp

--- client/ui/src/main/kotlin/calebxzhou/rdi/model/McVersion.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/model/McVersion.kt
@@ -10,13 +10,13 @@ enum class McVersion(
     //预留多loader支持
     val loaderVersions: Map<ModLoader, ModLoader.Version>
 ) {
-    V201(
+    /*V201(
         "1.20.1",
         "https://piston-meta.mojang.com/v1/packages/9318a951bbc903b54a21463a7eb8c4d451f7b132/1.20.1.json",
         mapOf(
             ModLoader.FORGE to ModLoader.Version("1.20.1-forge-47.4.13","https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.13/forge-1.20.1-47.4.13-installer.jar","790949ee0cb4671175a806befa370d69008b4b4e")
         )
-    ),
+    ),*/
     V211(
         "1.21.1" ,
         "https://piston-meta.mojang.com/v1/packages/a56257b4bc475ecac33571b51b68b33ac046fc72/1.21.1.json",
