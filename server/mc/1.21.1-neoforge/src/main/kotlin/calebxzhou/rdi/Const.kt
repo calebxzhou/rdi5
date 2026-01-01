0f0e1e75a67469e3d8f820ad985be8a9991d75b5
calebxzhou <me@calebxzhou.cn>
2026-01-01 23:22:30 +0800

    use mc default port

--- server/mc/1.21.1-neoforge/src/main/kotlin/calebxzhou/rdi/Const.kt
+++ server/mc/1.21.1-neoforge/src/main/kotlin/calebxzhou/rdi/Const.kt
@@ -2,8 +2,6 @@ package calebxzhou.rdi
 
 object Const {
     val DEBUG = System.getProperty("rdi.debug").toBoolean()
-    @JvmField
-    val SERVER_PORT = System.getenv("GAME_PORT")?.toIntOrNull()?:System.getProperty("rdi.port")?.toIntOrNull()?:65232
     val TEST_HOST_ID = "691bf5ef6cf16c0ae4224c31"
 
 
