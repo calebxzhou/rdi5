5c2ddf1d38aa1f5495f3dd457e931690dd97fb6e
calebxzhou <me@calebxzhou.cn>
2026-01-01 23:08:55 +0800

    store version metadata inside jar

--- client/ui/src/main/kotlin/calebxzhou/rdi/model/McVersion.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/model/McVersion.kt
@@ -1,12 +1,12 @@
 package calebxzhou.rdi.model
 
+import calebxzhou.mykotutils.std.jarResource
 import calebxzhou.rdi.common.model.ModLoader
 import calebxzhou.rdi.common.serdesJson
 import calebxzhou.rdi.service.GameService
 
 enum class McVersion(
     val mcVer: String,
-    val metaUrl: String,
     //预留多loader支持
     val loaderVersions: Map<ModLoader, ModLoader.Version>
 ) {
@@ -19,16 +19,18 @@ enum class McVersion(
     ),*/
     V211(
         "1.21.1" ,
-        "https://piston-meta.mojang.com/v1/packages/a56257b4bc475ecac33571b51b68b33ac046fc72/1.21.1.json",
+       // "https://piston-meta.mojang.com/v1/packages/a56257b4bc475ecac33571b51b68b33ac046fc72/1.21.1.json",
         mapOf(
             ModLoader.NEOFORGE to ModLoader.Version("neoforge-21.1.216","https://maven.neoforged.net/releases/net/neoforged/neoforge/21.1.216/neoforge-21.1.216-installer.jar","62118f0aed41e5a5c2b1b9e4feb35a22c4609548")
         )
     )
     ;
+    val metadata get() = this.jarResource("mcmeta/$mcVer.json").readAllBytes().decodeToString().let { serdesJson.decodeFromString<MojangVersionManifest>(it) }
     val baseDir get() = GameService.versionListDir.resolve(mcVer)
     val nativesDir get() = baseDir.resolve("natives")
     val firstLoader get() = loaderVersions.values.first()
-    val manifest: MojangVersionManifest get() = GameService.versionListDir.resolve(mcVer).resolve("$mcVer.json").let { serdesJson.decodeFromString(it.readText()) }
+    val firstLoaderDir get() = loaderVersions.values.first().let { GameService.versionListDir.resolve(it.id) }
+    val manifest: MojangVersionManifest get() = metadata//GameService.versionListDir.resolve(mcVer).resolve("$mcVer.json").let { serdesJson.decodeFromString(it.readText()) }
     val loaderManifest: MojangVersionManifest get() = GameService.versionListDir.resolve(firstLoader.id).resolve(firstLoader.id + ".json").let { serdesJson.decodeFromString(it.readText()) }
 
     companion object{
