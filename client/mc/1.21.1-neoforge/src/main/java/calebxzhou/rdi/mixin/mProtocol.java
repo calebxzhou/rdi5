c0496fabb3c44f78f943fdccc4433c8db97ecafd
calebxzhou <me@calebxzhou.cn>
2026-01-04 20:41:38 +0800

    merge protocol mixins

---
+++ client/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mProtocol.java
@@ -0,0 +1,38 @@
+package calebxzhou.rdi.mixin;
+
+import calebxzhou.rdi.client.mc.RDI;
+import net.minecraft.network.protocol.handshake.ClientIntent;
+import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
+import net.minecraft.network.protocol.login.ServerboundHelloPacket;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * calebxzhou @ 2025-10-21 10:33
+ */
+@Mixin(ServerboundHelloPacket.class)
+class mServerboundHelloPacket {
+    @ModifyConstant(method = "write",constant = @Constant(intValue = 16))
+    private static int RDI$WiderLengthForChineseName(int constant){
+        return 64;
+    }
+}
+@Mixin(ClientIntentionPacket.class)
+class mClientIntentionPacket {
+    @Shadow
+    @Final
+    @Mutable
+    private int port;
+
+    @Inject(method = "<init>(ILjava/lang/String;ILnet/minecraft/network/protocol/handshake/ClientIntent;)V",at=@At("TAIL"))
+    private void RDI$InjectHostPort(int protocolVersion, String hostName, int port, ClientIntent intention, CallbackInfo ci){
+        this.port = RDI.HOST_PORT;
+    }
+}
