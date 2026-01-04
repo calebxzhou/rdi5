b9a555e5e69f8b3d76251a8310c9116d8dc0f13f
calebxzhou <me@calebxzhou.cn>
2026-01-04 20:41:26 +0800

    merge protocol mixins

--- server/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mProtocol.java
+++ server/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mProtocol.java
@@ -1,24 +1,44 @@
 package calebxzhou.rdi.mixin;
 
-import net.minecraft.network.FriendlyByteBuf;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.network.protocol.login.ServerboundHelloPacket;
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Optional;
+import javax.annotation.Nullable;
 
 /**
  * calebxzhou @ 2024-05-31 20:09
  */
 @Mixin(ServerboundHelloPacket.class)
-abstract class mLoginProtocol1 {
+abstract class mAllowChineseNameLogin {
 
-    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V",constant = @Constant(intValue = 16))
-    private static int RDI$AllowChineseName(int constant){
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 16))
+    private static int RDI$AllowChineseName(int constant) {
         return 64;
     }
 
+}
+/**
+ * calebxzhou @ 2025-02-21 0:32
+ */
+@Mixin(ServerLoginPacketListenerImpl.class)
+class mServerLoginPacketListener {
+    @Shadow
+    @Nullable
+    @Mutable
+    private GameProfile authenticatedProfile;
+
+    //注入rdid到mc本体的uuid
+    @Inject(method = "handleHello", at = @At(value = "TAIL"))
+    private void injectUUID(ServerboundHelloPacket pPacket, CallbackInfo ci) {
+        authenticatedProfile = new GameProfile(pPacket.profileId(), pPacket.name());
+    }
 }
