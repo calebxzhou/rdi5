f13e5bcddaa452a2aaf9bc4a38284494e4d0c436
calebxzhou <me@calebxzhou.cn>
2026-01-04 23:46:34 +0800

    write port

--- client/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mProtocol.java
+++ client/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mProtocol.java
@@ -1,6 +1,7 @@
 package calebxzhou.rdi.mixin;
 
 import calebxzhou.rdi.client.mc.RDI;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.handshake.ClientIntent;
 import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
 import net.minecraft.network.protocol.login.ServerboundHelloPacket;
@@ -8,10 +9,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
@@ -34,5 +32,10 @@ class mClientIntentionPacket {
     @Inject(method = "<init>(ILjava/lang/String;ILnet/minecraft/network/protocol/handshake/ClientIntent;)V",at=@At("TAIL"))
     private void RDI$InjectHostPort(int protocolVersion, String hostName, int port, ClientIntent intention, CallbackInfo ci){
         this.port = RDI.HOST_PORT;
+    }
+    @Redirect(method = "write",at= @At(value = "INVOKE", target = "Lnet/minecraft/network/FriendlyByteBuf;writeShort(I)Lnet/minecraft/network/FriendlyByteBuf;"))
+    private FriendlyByteBuf RDI$WriteCorrectPort(FriendlyByteBuf buf, int value){
+        buf.writeShort(RDI.HOST_PORT);
+        return buf;
     }
 }
