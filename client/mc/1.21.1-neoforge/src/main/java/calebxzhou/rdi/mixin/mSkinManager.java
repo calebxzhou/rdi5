02b4c62f432129615e19d0082bc73e9e0993c158
calebxzhou <me@calebxzhou.cn>
2026-01-06 18:50:00 +0800

    opt import

--- client/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mSkinManager.java
+++ client/mc/1.21.1-neoforge/src/main/java/calebxzhou/rdi/mixin/mSkinManager.java
@@ -4,23 +4,12 @@ import calebxzhou.rdi.client.mc.RDI;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.gson.Gson;
-import com.mojang.authlib.minecraft.MinecraftProfileTextures;
-import com.mojang.authlib.properties.Property;
-import net.minecraft.Util;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.client.resources.SkinManager;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 /**
