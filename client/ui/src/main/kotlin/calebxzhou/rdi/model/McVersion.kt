61272845a225c6bc7c46d895d0e0e6aa21d33b6d
calebxzhou <me@calebxzhou.cn>
2026-01-07 23:31:37 +0800

    move mcversion to common

--- client/ui/src/main/kotlin/calebxzhou/rdi/model/McVersion.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/model/McVersion.kt
@@ -2,40 +2,21 @@ package calebxzhou.rdi.model
 
 import calebxzhou.mykotutils.std.jarResource
 import calebxzhou.mykotutils.std.readAllString
+import calebxzhou.rdi.common.model.McVersion
 import calebxzhou.rdi.common.model.ModLoader
 import calebxzhou.rdi.common.serdesJson
 import calebxzhou.rdi.service.GameService
 
-enum class McVersion(
-    val mcVer: String,
-    //预留多loader支持
-    val loaderVersions: Map<ModLoader, ModLoader.Version>
-) {
-    V201(
-        "1.20.1",
-       // "https://piston-meta.mojang.com/v1/packages/9318a951bbc903b54a21463a7eb8c4d451f7b132/1.20.1.json",
-        mapOf(
-            ModLoader.FORGE to ModLoader.Version("1.20.1-forge-47.4.13","https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.13/forge-1.20.1-47.4.13-installer.jar","790949ee0cb4671175a806befa370d69008b4b4e")
-        )
-    ),
-    V211(
-        "1.21.1" ,
-       // "https://piston-meta.mojang.com/v1/packages/a56257b4bc475ecac33571b51b68b33ac046fc72/1.21.1.json",
-        mapOf(
-            ModLoader.NEOFORGE to ModLoader.Version("neoforge-21.1.216","https://maven.neoforged.net/releases/net/neoforged/neoforge/21.1.216/neoforge-21.1.216-installer.jar","62118f0aed41e5a5c2b1b9e4feb35a22c4609548")
-        )
-    )
-    ;
-    val metadata get() = this.jarResource("mcmeta/$mcVer.json").readAllString().let { serdesJson.decodeFromString<MojangVersionManifest>(it) }
-    val baseDir get() = GameService.versionListDir.resolve(mcVer)
-    val nativesDir get() = baseDir.resolve("natives")
-    val firstLoader get() = loaderVersions.keys.first()
-    val firstLoaderVersion get() = loaderVersions.values.first()
-    val firstLoaderDir get() = loaderVersions.values.first().let { GameService.versionListDir.resolve(it.id) }
-    val manifest: MojangVersionManifest get() = metadata//GameService.versionListDir.resolve(mcVer).resolve("$mcVer.json").let { serdesJson.decodeFromString(it.readText()) }
-    val loaderManifest: MojangVersionManifest get() = GameService.versionListDir.resolve(firstLoaderVersion.id).resolve(firstLoaderVersion.id + ".json").let { serdesJson.decodeFromString(it.readText()) }
+val McVersion.metadata
+    get() = this.jarResource("mcmeta/$mcVer.json").readAllString()
+        .let { serdesJson.decodeFromString<MojangVersionManifest>(it) }
+val McVersion.baseDir get() = GameService.versionListDir.resolve(mcVer)
+val McVersion.nativesDir get() = baseDir.resolve("natives")
+val McVersion.firstLoader get() = loaderVersions.keys.first()
+val McVersion.firstLoaderVersion get() = loaderVersions.values.first()
+val McVersion.firstLoaderDir get() = loaderVersions.values.first().let { GameService.versionListDir.resolve(it.id) }
+val McVersion.manifest: MojangVersionManifest get() = metadata//GameService.versionListDir.resolve(mcVer).resolve("$mcVer.json").let { serdesJson.decodeFromString(it.readText()) }
+val McVersion.loaderManifest: MojangVersionManifest
+    get() = GameService.versionListDir.resolve(firstLoaderVersion.id).resolve(firstLoaderVersion.id + ".json")
+        .let { serdesJson.decodeFromString(it.readText()) }
 
-    companion object{
-        fun from(mcVer: String): McVersion? = entries.firstOrNull { it.mcVer == mcVer }
-    }
-}
--- server/master/src/main/kotlin/calebxzhou/rdi/master/model/McVersion.kt
+++
@@ -1,6 +0,0 @@
-package calebxzhou.rdi.master.model
-
-enum class McVersion(val verStr: String) {
-    V211("1.21.1"),
-    V201("1.20.1"),
-}
