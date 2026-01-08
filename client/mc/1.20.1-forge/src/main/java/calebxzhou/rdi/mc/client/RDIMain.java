458013ec88f599a4a8170473cc038dd64c1e70ea
calebxzhou <me@calebxzhou.cn>
2026-01-08 23:57:58 +0800

    start log

--- client/mc/1.20.1-forge/src/main/java/calebxzhou/rdi/mc/client/RDIMain.java
+++ client/mc/1.20.1-forge/src/main/java/calebxzhou/rdi/mc/client/RDIMain.java
@@ -1,11 +1,15 @@
 package calebxzhou.rdi.mc.client;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
 
 /**
  * calebxzhou @ 2026-01-06 19:19
  */
 @Mod("rdi")
 public class RDIMain {
-
+    public RDIMain() {
+        LogManager.getLogger("rdi").info("❄❄❄❄❄❄❄❄RDI客户端核心模块已加载❄❄❄❄❄❄❄❄");
+    }
 }
