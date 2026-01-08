1a82b26129a71abdc01ce40c38212a872a48495c
calebxzhou <me@calebxzhou.cn>
2026-01-08 23:57:53 +0800

    bigger conn btn

--- client/mc/1.20.1-forge/src/main/java/calebxzhou/rdi/mc/client/mixin/mTitleScreen.java
+++ client/mc/1.20.1-forge/src/main/java/calebxzhou/rdi/mc/client/mixin/mTitleScreen.java
@@ -10,7 +10,6 @@ import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.multiplayer.resolver.ServerAddress;
 import net.minecraft.client.renderer.PanoramaRenderer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -38,22 +37,18 @@ public class mTitleScreen extends Screen {
         guiGraphics.blit(ResourceLocation.tryBuild("rdi", "textures/bg/1.jpg"), 0, 0, 0, 0, this.width, this.height, this.width, this.height);
     }
 
-    @Redirect(method = "createNormalMenuOptions", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/network/chat/Component;translatable(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;"))
-    private MutableComponent RDI$AddMultiplayerButton(String key) {
-
-        return Component.literal("进入主机：" + HOST_NAME);
+    @Inject(method = "createNormalMenuOptions", at = @At("HEAD"))
+    private void RDI$AddMultiplayerButton(int y, int rowHeight, CallbackInfo ci) {
+         addRenderableWidget(Button.builder(Component.literal("进入主机：" + HOST_NAME),(btn)->{
+            HostAndPort hp = HostAndPort.fromString(GAME_IP);
+            ConnectScreen.startConnecting(
+                    new TitleScreen(),
+                    this.getMinecraft(),
+                    new ServerAddress(hp.getHost(), hp.getPort()),
+                    new ServerData("rdi", GAME_IP, false),
+                    false
+            );
+        }).bounds(0,0,width,100).build());
     }
 
-    @Inject(method = "lambda$createNormalMenuOptions$8", at = @At("HEAD"), cancellable = true)
-    private void RDI$onClickMultiplayer(Button p_280833_, CallbackInfo ci) {
-        HostAndPort hp = HostAndPort.fromString(GAME_IP);
-        ConnectScreen.startConnecting(
-                new TitleScreen(),
-                this.getMinecraft(),
-                new ServerAddress(hp.getHost(), hp.getPort()),
-                new ServerData("rdi", GAME_IP, false),
-                false
-        );
-        ci.cancel();
-    }
 }
