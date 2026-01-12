278ae580f1b02b481bb75f0cf2d80f13dd2afb1d
calebxzhou <me@calebxzhou.cn>
2026-01-13 00:09:24 +0800

    firewall limit outbound data

--- server/base-image/j21/start-server.sh
+++ server/base-image/j21/start-server.sh
@@ -5,6 +5,30 @@ START_PARAMS="${START_PARAMS:-"-Xmx8G @libraries/net/neoforged/neoforge/21.1.217
 
 ls -la /opt/server >&2
 chown -R rdi:rdi /home/rdi /opt/server /data
+
+# Restrict outgoing connections to LAN and Localhost only, but allow replies to incoming
+echo "Applying firewall rules..."
+# Flush existing rules to be safe
+iptables -F INPUT
+iptables -F OUTPUT
+
+# Allow all incoming connections (default is usually ACCEPT, but being explicit)
+iptables -P INPUT ACCEPT
+
+# Allow loopback
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow established and related connections (allows replies to incoming connections from anywhere)
+iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# Allow outgoing connections to standard private networks (RFC 1918)
+iptables -A OUTPUT -d 10.0.0.0/8 -j ACCEPT
+iptables -A OUTPUT -d 172.16.0.0/12 -j ACCEPT
+iptables -A OUTPUT -d 192.168.0.0/16 -j ACCEPT
+
+# Set default policy to DROP for other outgoing connections
+iptables -P OUTPUT DROP
+
 # Drop to the non-root server user before launching Java
 cd /opt/server
 eval "JAVA_ARGS=(${START_PARAMS})"
