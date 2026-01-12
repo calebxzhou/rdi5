54f3e3c848878536e235768e974b4d7bfff42708
calebxzhou <me@calebxzhou.cn>
2026-01-13 00:08:38 +0800

    merged to ui

--- client/ui2/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-    }
-}
-rootProject.name = "ui2"
-include(":common")
-project(":common").projectDir = file("../../common")
