4923e1f0428be0d9398c7fba4121ccc8e1d95616
calebxzhou <me@calebxzhou.cn>
2026-01-15 23:54:31 +0800

    impl later

--- client/ui/src/main/kotlin/calebxzhou/rdi/client/ui2/screen/HostInfoScreen.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/client/ui2/screen/HostInfoScreen.kt
@@ -3,9 +3,11 @@ package calebxzhou.rdi.client.ui2.screen
 import androidx.compose.runtime.Composable
 
 /**
- * calebxzhou @ 2026-01-12 17:07
+ * calebxzhou @ 2026-01-15 19:38
  */
 @Composable
-fun SkinInfoScreen(){
+fun HostInfoScreen(
+    onBack: () -> Unit = {}
+){
 
 }
