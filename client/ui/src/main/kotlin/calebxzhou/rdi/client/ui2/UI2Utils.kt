9c8686458de7af6054359f0330c54ea30f40b3d5
calebxzhou <me@calebxzhou.cn>
2026-01-17 20:00:52 +0800

    reformat code

--- client/ui/src/main/kotlin/calebxzhou/rdi/client/ui2/UI2Utils.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/client/ui2/UI2Utils.kt
@@ -1,29 +1,15 @@
 package calebxzhou.rdi.client.ui2
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarHost
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.TextButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.PlainTooltip
@@ -31,15 +17,15 @@ import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -50,32 +36,40 @@ import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import calebxzhou.mykotutils.std.jarResource
 import calebxzhou.rdi.RDIClient
+import kotlinx.coroutines.launch
 import org.jetbrains.skia.Image
 import javax.swing.JOptionPane
 
 /**
  * calebxzhou @ 2026-01-12 21:11
  */
-val DEFAULT_MODPACK_ICON =Image.makeFromEncoded(RDIClient.jarResource("assets/icons/modpack.png").use { it.readBytes() }).toComposeImageBitmap()
-val DEFAULT_HOST_ICON =Image.makeFromEncoded(RDIClient.jarResource("assets/icons/host.png").use { it.readBytes() }).toComposeImageBitmap()
+val DEFAULT_MODPACK_ICON =
+    Image.makeFromEncoded(RDIClient.jarResource("assets/icons/modpack.png").use { it.readBytes() })
+        .toComposeImageBitmap()
+val DEFAULT_HOST_ICON =
+    Image.makeFromEncoded(RDIClient.jarResource("assets/icons/host.png").use { it.readBytes() }).toComposeImageBitmap()
+
 @Composable
-fun alertOk(msg: String){
+fun alertOk(msg: String) {
     val title = "成功"
     val icon = "\uF058"
     createAlertDialog(title = title, icon = icon, msg = msg, accentColor = MaterialColor.GREEN_900.color)
 }
+
 @Composable
-fun alertWarn(msg: String){
+fun alertWarn(msg: String) {
     val title = "警告"
     val icon = "\uEA6C"
     createAlertDialog(title = title, icon = icon, msg = msg, accentColor = Color(0xFFE0A800))
 }
+
 @Composable
-fun alertErr(msg: String){
+fun alertErr(msg: String) {
     val title = "错误"
     val icon = "\uEA87"
     createAlertDialog(title = title, icon = icon, msg = msg, accentColor = Color(0xFFD64545))
 }
+
 fun alertErrOs(msg: String) {
     JOptionPane.showMessageDialog(
         null,
@@ -84,13 +78,14 @@ fun alertErrOs(msg: String) {
         JOptionPane.ERROR_MESSAGE
     )
 }
+
 @Composable
 private fun createAlertDialog(
     title: String,
     icon: String,
     msg: String,
     accentColor: Color
-){
+) {
     var visible by remember { mutableStateOf(true) }
     if (!visible) return
 
@@ -138,6 +133,7 @@ private fun createAlertDialog(
         shape = RoundedCornerShape(28.dp)
     )
 }
+
 //pua codepoint识别并应用图标字体
 val String.asIconText
     get() = buildAnnotatedString {
@@ -169,9 +165,14 @@ val Int.wM: Modifier
 
 val Int.hM: Modifier
     get() = Modifier.height(this.dp)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SimpleTooltip(text: String, position: TooltipAnchorPosition = TooltipAnchorPosition.Above, content: @Composable (() -> Unit)){
+fun SimpleTooltip(
+    text: String,
+    position: TooltipAnchorPosition = TooltipAnchorPosition.Above,
+    content: @Composable (() -> Unit)
+) {
     val state = rememberTooltipState()
     TooltipBox(
         positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
@@ -183,7 +184,7 @@ fun SimpleTooltip(text: String, position: TooltipAnchorPosition = TooltipAnchorP
                 caretShape = null,
                 containerColor = MaterialColor.GRAY_200.color
             ) {
-                Text(text, fontSize = TextUnit(12f, TextUnitType.Sp),color = Color.Black)
+                Text(text, fontSize = TextUnit(12f, TextUnitType.Sp), color = Color.Black)
             }
         },
         state = state
@@ -286,16 +287,26 @@ fun CircleIconButton(
     } ?: drawButton()
 }
 @Composable
-fun MainColumn(content: @Composable (ColumnScope.() -> Unit)){
-    Column(modifier = Modifier.fillMaxSize().padding(start=24.dp, end = 24.dp, top=12.dp, bottom=8.dp)){ content() }
+fun MainColumn(content: @Composable (ColumnScope.() -> Unit)) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(start = 24.dp, end = 24.dp, top = 12.dp, bottom = 8.dp)
+    ) { content() }
 }
 
 @Composable
-fun MainBox(content: @Composable (BoxScope.() -> Unit)){
-    Box(modifier = Modifier.fillMaxSize() ){ content() }
+fun MainBox(content: @Composable (BoxScope.() -> Unit)) {
+    Box(modifier = Modifier.fillMaxSize()) { content() }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TitleRow(title: String, onBack: () -> Unit, content: @Composable (RowScope.() -> Unit)){
+fun TitleRow(
+    title: String,
+    onBack: () -> Unit,
+    longPressToBack: Boolean = false,
+    content: @Composable (RowScope.() -> Unit)
+) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
@@ -315,7 +326,7 @@ fun TitleRow(title: String, onBack: () -> Unit, content: @Composable (RowScope.(
     }
 }
 @Composable
-fun BoxScope.BottomSnakebar(state: SnackbarHostState){
+fun BoxScope.BottomSnakebar(state: SnackbarHostState) {
     SnackbarHost(
         hostState = state,
         modifier = Modifier.align(Alignment.BottomCenter).padding(16.dp)
