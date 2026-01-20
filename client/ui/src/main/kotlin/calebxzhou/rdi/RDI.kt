b8b8531844e6f47cde5d4cbc3924e4bd0f786ab5
calebxzhou <me@calebxzhou.cn>
2026-01-20 16:47:45 +0800

    move symlink to mykotutils + reformat

--- client/ui/src/main/kotlin/calebxzhou/rdi/RDI.kt
+++ client/ui/src/main/kotlin/calebxzhou/rdi/RDI.kt
@@ -1,32 +1,21 @@
 package calebxzhou.rdi
 
 import calebxzhou.rdi.client.AppConfig
-import calebxzhou.rdi.client.ui.RodernUI
-import calebxzhou.rdi.client.ui.component.alertErrOs
-import icyllis.modernui.R
-import icyllis.modernui.audio.AudioManager
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.slf4j.MarkerFactory
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
-import kotlin.concurrent.thread
 
 val CONF = AppConfig.load()
-val lgr = KotlinLogging.logger {  }
-val logMarker
-    get() = {marker: String ->  MarkerFactory.getMarker(marker)}
-fun main(){
+val lgr = KotlinLogging.logger { }
+fun main() {
     //redirect new ui
-    calebxzhou.rdi.client.ui2.main()
-    //RDIClient.start(_root_ide_package_.calebxzhou.rdi.client.ui.frag.TitleFragment())
+    calebxzhou.rdi.client.main()
 }
+
 object RDIClient {
-
-
-        val DIR: File = File(System.getProperty("user.dir")).absoluteFile
-
-
+    val DIR: File = File(System.getProperty("user.dir")).absoluteFile
 
     init {
         lgr.info { "RDI启动中" }
@@ -35,23 +24,6 @@ object RDIClient {
 
     }
 
-    }
-
-    private fun canCreateSymlink(): Boolean {
-        val target = runCatching { Files.createTempFile(DIR.toPath(), "symlink-test-target", ".tmp") }.getOrNull()
-            ?: return true
-        val link: Path = DIR.toPath().resolve("symlink-test-link-${System.currentTimeMillis()}")
-        return runCatching {
-            Files.deleteIfExists(link)
-            Files.createSymbolicLink(link, target)
-            true
-        }.onFailure { err ->
-            lgr.warn(err) { "符号链接创建失败" }
-        }.getOrElse { false }.also {
-            runCatching { Files.deleteIfExists(link) }
-            runCatching { Files.deleteIfExists(target) }
-        }
-    }
 }
 
 
