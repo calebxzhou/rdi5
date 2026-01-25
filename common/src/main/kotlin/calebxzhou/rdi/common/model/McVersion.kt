63ab0079b865f063aa6758ac87bc20594016ab8f
calebxzhou <me@calebxzhou.cn>
2026-01-25 18:18:00 +0800

    jre ver

--- common/src/main/kotlin/calebxzhou/rdi/common/model/McVersion.kt
+++ common/src/main/kotlin/calebxzhou/rdi/common/model/McVersion.kt
@@ -3,6 +3,7 @@ package calebxzhou.rdi.common.model
 enum class McVersion(
     val mcVer: String,
     val icon : String,
+    val jreVer: Int,
     //预留多loader支持
     val loaderVersions: Map<ModLoader, ModLoader.Version>
 ) {
@@ -10,6 +11,7 @@ enum class McVersion(
     V211(
         "1.21.1" ,
         "assets/icons/mace.png",
+        21,
         // "https://piston-meta.mojang.com/v1/packages/a56257b4bc475ecac33571b51b68b33ac046fc72/1.21.1.json",
         mapOf(
             ModLoader.neoforge to ModLoader.Version("neoforge-21.1.216","https://maven.neoforged.net/releases/net/neoforged/neoforge/21.1.216/neoforge-21.1.216-installer.jar","62118f0aed41e5a5c2b1b9e4feb35a22c4609548")
@@ -17,7 +19,7 @@ enum class McVersion(
     ),
     V201(
         "1.20.1",
-        "assets/icons/brush.png",
+        "assets/icons/brush.png",21,
         // "https://piston-meta.mojang.com/v1/packages/9318a951bbc903b54a21463a7eb8c4d451f7b132/1.20.1.json",
         mapOf(
             ModLoader.forge to ModLoader.Version("1.20.1-forge-47.4.13","https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.13/forge-1.20.1-47.4.13-installer.jar","790949ee0cb4671175a806befa370d69008b4b4e")
@@ -25,7 +27,7 @@ enum class McVersion(
     ),
     V182(
         "1.18.2",
-        "assets/icons/copper.png",
+        "assets/icons/copper.png",21,
         //https://piston-meta.mojang.com/v1/packages/334b33fcba3c9be4b7514624c965256535bd7eba/1.18.2.json
         mapOf(
             ModLoader.forge to ModLoader.Version("1.18.2-forge-40.3.12","https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.3.12/forge-1.18.2-40.3.12-installer.jar","d7f759dec5b52ddb342c3e12511da1674d0401bf")
