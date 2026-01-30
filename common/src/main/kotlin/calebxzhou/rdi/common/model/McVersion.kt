772964f15033be628626541d708aedb56d615425
calebxzhou <me@calebxzhou.cn>
2026-01-30 19:20:02 +0800

    neoforge ver up

--- common/src/main/kotlin/calebxzhou/rdi/common/model/McVersion.kt
+++ common/src/main/kotlin/calebxzhou/rdi/common/model/McVersion.kt
@@ -1,5 +1,6 @@
 package calebxzhou.rdi.common.model
 
+//https://bmclapi2.bangbang93.com/mc/game/version_manifest_v2.json
 enum class McVersion(
     val mcVer: String,
     val icon : String,
@@ -14,7 +15,7 @@ enum class McVersion(
         21,
         // "https://piston-meta.mojang.com/v1/packages/a56257b4bc475ecac33571b51b68b33ac046fc72/1.21.1.json",
         mapOf(
-            ModLoader.neoforge to ModLoader.Version("neoforge-21.1.216","https://maven.neoforged.net/releases/net/neoforged/neoforge/21.1.216/neoforge-21.1.216-installer.jar","62118f0aed41e5a5c2b1b9e4feb35a22c4609548")
+            ModLoader.neoforge to ModLoader.Version("neoforge-21.1.219","https://maven.neoforged.net/releases/net/neoforged/neoforge/21.1.219/neoforge-21.1.219-installer.jar","832c9404c9305da6096829729bc8a94c0da60087")
         )
     ),
     V201(
